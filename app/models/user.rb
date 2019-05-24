acbb521a783383b39c15b5eb47db5293af475d2c
eramuk <eramuk@gmail.com>
2019-05-24 23:57:27 +0900

    add password to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  has_secure_password
 end
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,5 +2,7 @@ FactoryBot.define do
   factory :user do
     name { "alice" }
     email { "alice@example.com" }
+    password { "alice" }
+    password_confirmation { "alice" }
   end
 end
