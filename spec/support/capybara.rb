2a2f463d2f6e5ae6b931934f11fb22f00ecf9b04
eramuk <eramuk@gmail.com>
2019-05-29 22:35:26 +0900

    fix chromedriver

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -2,21 +2,21 @@ require 'selenium-webdriver'
 require 'capybara/rspec'
 
 Capybara.configure do |capybara_config|
-  capybara_config.default_driver = :selenium_chrome
+  capybara_config.default_driver = :selenium_chrome_headless
   capybara_config.default_max_wait_time = 5
 end
 
-Capybara.register_driver :selenium_chrome do |app|
+Capybara.register_driver :selenium_chrome_headless do |app|
+  Selenium::WebDriver::Chrome::Service.driver_path = "/mnt/c/Windows/chromedriver.exe"
   Capybara::Selenium::Driver.new(app,
     browser: :chrome,
     desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
       chromeOptions: {
-        binary: "/usr/bin/chromium-browser",
-        args: %w(headless no-sandbox  disable-gpu window-size=1680,1050),
+        args: %w(headless no-sandbox disable-gpu window-size=1680,1050),
       }
     )
   )
 end
 
-Capybara.default_driver = :selenium_chrome
-Capybara.javascript_driver = :selenium_chrome
+Capybara.default_driver = :selenium_chrome_headless
+Capybara.javascript_driver = :selenium_chrome_headless
