e1d522a7dc117bf92cefb563eabed892bded4bc4
eramuk <eramuk@gmail.com>
2019-05-30 01:24:07 +0900

    add session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root 'login#index'
+
   get 'signup' => 'users#new'
+
+  get    '/login',  to: 'sessions#new'
+  post   '/login',  to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   resources :users
 end
