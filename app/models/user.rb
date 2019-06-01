7a0b5955a61e67d503c4a011206ca9a119e93b64
eramuk <eramuk@gmail.com>
2019-06-01 22:59:17 +0900

    add remember cookie

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,35 @@
 class User < ApplicationRecord
-  has_secure_password
+  attr_accessor :remember_token
 
   PASSWORD_MIN_LENGTH = 8
+
+  has_secure_password
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, presence: true, length: { minimum: PASSWORD_MIN_LENGTH }
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 end
