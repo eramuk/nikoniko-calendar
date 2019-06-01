f5a99390bfdee413da5711aebed7291f6e2e16c1
eramuk <eramuk@gmail.com>
2019-06-01 22:00:08 +0900

    add redirect current user

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,4 +1,7 @@
 class LoginController < ApplicationController
   def index
+    if logged_in?
+      redirect_to current_user
+    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       redirect_to @user
     else
       flash.now[:danger] = @user.errors.full_messages.uniq.join("\n")
