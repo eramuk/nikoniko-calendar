72852afd191968a19917b1a45cf980ca5e3e788a
eramuk <eramuk@gmail.com>
2019-07-13 02:51:15 +0900

    refactor Mood::Score

--- app/models/mood.rb
+++ app/models/mood.rb
@@ -3,13 +3,21 @@ class Mood < ApplicationRecord
     GOOD = 1
     NORMAL = 2
     BAD = 3
+
+    def self.list
+      [GOOD, NORMAL, BAD]
+    end
+
+    def self.image_path(score)
+      ["mood_good.png", "mood_normal.png", "mood_bad.png"][score - 1]
+    end
   end
 
   belongs_to :user
 
   validates :user_id, presence: true, uniqueness: { scope: :date }
   validates :date, presence: true
-  validates :score, presence: true, inclusion: { in: Score::GOOD..Score::BAD }
+  validates :score, presence: true, inclusion: { in: Score::list }
 
   scope :recent_week, -> (num) { where(date: (num.week.ago + 1.day)..Time.current) }
 end
