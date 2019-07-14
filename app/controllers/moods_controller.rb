0cf140bc5a24d0f72aeaca688074ae87405aea78
eramuk <eramuk@gmail.com>
2019-07-15 03:31:33 +0900

    add update for mood

--- app/controllers/moods_controller.rb
+++ app/controllers/moods_controller.rb
@@ -1,10 +1,16 @@
 class MoodsController < ApplicationController
-  before_action :logged_in_user, only: [:create]
+  before_action :logged_in_user
 
   def create
     @mood = current_user.moods.build(mood_params)
     flash[:alert] = "Faild to post mood" unless @mood.save
     redirect_to root_url
+  end
+
+  def update
+    @mood = current_user.moods.find(params[:id])
+    flash[:alert] = "Faild to post mood" unless @mood.update(score: mood_params[:score])
+    redirect_to "/users/1"
   end
 
   private
