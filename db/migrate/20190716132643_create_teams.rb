4d3fb047799ee4d2e040cc79c158d7f72bac51e3
eramuk <eramuk@gmail.com>
2019-07-22 01:09:43 +0900

    fix migrate

--- db/migrate/20190716132643_create_teams.rb
+++ db/migrate/20190716132643_create_teams.rb
@@ -3,7 +3,7 @@ class CreateTeams < ActiveRecord::Migration[5.2]
     create_table :teams do |t|
       t.bigint :user_id
       t.text :name
-      t.index ["user_id", "name"], unique: true
+      t.index ["name"], unique: true
       t.timestamps
     end
   end
--- db/migrate/20190717131146_rename_index_to_teams.rb
+++
@@ -1,5 +0,0 @@
-class RenameIndexToTeams < ActiveRecord::Migration[5.2]
-  def change
-    rename_index :teams, 'index_teams_on_user_id_and_name', 'index_teams_on_name'
-  end
-end
