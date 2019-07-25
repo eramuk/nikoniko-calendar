a9b67b4c897b4f93beb0135539d998f8b33f80dc
eramuk <eramuk@gmail.com>
2019-07-26 01:32:20 +0900

    add edit team

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -14,9 +14,23 @@ class TeamsController < ApplicationController
     @team = current_user.teams.build(team_params)
     if current_user.save
       flash[:notice] = "Successfully created"
-      redirect_to controller: 'teams', action: 'index'
+      redirect_to controller: "teams", action: "index"
     else
       render "new"
+    end
+  end
+
+  def edit
+    @team = current_user.teams.find(params[:id])
+  end
+
+  def update
+    @team = current_user.teams.find(params[:id])
+    if @team.update_attributes(team_params)
+      flash[:notice] = "Successfully updated"
+      redirect_to controller: "teams", action: "index"
+    else
+      render "edit"
     end
   end
 
