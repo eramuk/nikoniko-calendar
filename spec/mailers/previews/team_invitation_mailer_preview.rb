4c5f81040311cdd771db423b591fa957387aeab5
eramuk <eramuk@gmail.com>
2019-07-28 23:38:21 +0900

    add team_invitation_mailer

--- spec/mailers/previews/team_invitation_mailer_preview.rb
+++ spec/mailers/previews/team_invitation_mailer_preview.rb
@@ -1,4 +1,12 @@
 # Preview all emails at http://localhost:3000/rails/mailers/team_invitation_mailer
 class TeamInvitationMailerPreview < ActionMailer::Preview
-
+  # Preview this email at http://localhost:3000/rails/mailers/team_invitation_mailer/activation
+  def activation
+    team = Team.first
+    sender = User.first
+    recipient = User.first
+    team_invitation = TeamInvitation.new(team_id: team.id, sender_id: sender.id, recipient_id: recipient.id)
+    team_invitation.activation_token = TeamInvitation.new_token
+    TeamInvitationMailer.activation(team_invitation)
+  end
 end
