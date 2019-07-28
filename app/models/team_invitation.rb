f8f57fb5291810726af1c9d8e50ae79a153f943f
eramuk <eramuk@gmail.com>
2019-07-28 13:26:31 +0900

    add validates to team_invitation

--- app/models/team_invitation.rb
+++ app/models/team_invitation.rb
@@ -1,2 +1,12 @@
 class TeamInvitation < ApplicationRecord
+  before_create :create_activation_digest
+  validates :team_id, presence: true
+  validates :sender_user_id, presence: true
+  validates :recipient_user_id, presence: true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  private
+
+  def create_activation_digest
+  end
 end
