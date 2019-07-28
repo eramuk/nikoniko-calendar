03711f6e5f0c48cd8f0a62ce78c415a1ee497e9b
eramuk <eramuk@gmail.com>
2019-07-28 17:01:21 +0900

    add activation_token to team_invitation

--- app/models/team_invitation.rb
+++ app/models/team_invitation.rb
@@ -1,12 +1,26 @@
 class TeamInvitation < ApplicationRecord
+  attr_accessor :activation_token
+
   before_create :create_activation_digest
   validates :team_id, presence: true
   validates :sender_user_id, presence: true
   validates :recipient_user_id, presence: true
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 
+  def TeamInvitation.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  def TeamInvitation.new_token
+    SecureRandom.urlsafe_base64
+  end
+
   private
 
   def create_activation_digest
+    self.activation_token  = TeamInvitation.new_token
+    self.activation_digest = TeamInvitation.digest(activation_token)
   end
 end
