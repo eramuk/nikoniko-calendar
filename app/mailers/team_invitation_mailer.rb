ca24211cc050797822dc9f623fc5759a124c1c2d
eramuk <eramuk@gmail.com>
2019-09-27 23:04:05 +0900

    Specify the application name by config gem

--- app/mailers/team_invitation_mailer.rb
+++ app/mailers/team_invitation_mailer.rb
@@ -1,6 +1,6 @@
 class TeamInvitationMailer < ApplicationMailer
   def activation(team_invitation)
     @team_invitation = team_invitation
-    mail to: @team_invitation.email, subject: "[#{ENV['APP_NAME']}] Team Invitation"
+    mail to: @team_invitation.email, subject: "[#{Settings.app_name}] Team Invitation"
   end
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,11 @@
 class UserMailer < ApplicationMailer
   def account_activation(user)
     @user = user
-    mail to: user.email, subject: "[#{ENV['APP_NAME']}] Account activation"
+    mail to: user.email, subject: "[#{Settings.app_name}] Account activation"
   end
 
   def password_reset(user)
     @user = user
-    mail to: user.email, subject: "[#{ENV['APP_NAME']}] Password reset"
+    mail to: user.email, subject: "[#{Settings.app_name}] Password reset"
   end
 end
